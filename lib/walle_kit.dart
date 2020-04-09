06e1d653b9059c25aa99028bbfef47d3db2df16d
v7lin <v7lin@qq.com>
2020-04-10 00:42:03 +0800

    walle

--- lib/walle_kit.dart
+++ lib/walle_kit.dart
@@ -4,7 +4,8 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class WalleKit {
-  static const MethodChannel _channel = MethodChannel('v7lin.github.io/walle_kit');
+  static const MethodChannel _channel =
+      MethodChannel('v7lin.github.io/walle_kit');
 
   static Future<String> getChannelId() async {
     assert(Platform.isAndroid);
