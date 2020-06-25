d809dd72b0769b80563c452d2608a93c0d0ce879
沉默的湮灰 <v7lin@qq.com>
2020-06-25 16:11:46 +0800

    Update WalleKitPlugin.java

--- android/src/main/java/io/github/v7lin/walle_kit/WalleKitPlugin.java
+++ android/src/main/java/io/github/v7lin/walle_kit/WalleKitPlugin.java
@@ -33,10 +33,6 @@ public class WalleKitPlugin implements FlutterPlugin, MethodCallHandler {
     }
 
     private Context applicationContext;
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
 
     @Override
